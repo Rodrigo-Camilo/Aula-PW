cda7b3e02dddc014a5ffd00f357f967144b35397
Rodrigo-Camilo <rodrigocamilo2006@gmail.com>
2024-03-20 21:16:08 -0300

    formularios

---
+++ app.js
@@ -0,0 +1,8 @@
+document.querySelector('#button').addEventListener('click', function() {
+    let nome = document.getElementById('nome').value;
+    let sobrenome = document.getElementById('sobrenome').value;
+    let idade = document.getElementById('idade').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+
+});
